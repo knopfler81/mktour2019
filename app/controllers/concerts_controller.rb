baa561d59a8b5c4cc7b8ff608fc33ee88ad152a5
Nelly Duclos <nellyduclos8@gmail.com>
2019-03-07 20:45:10 +0100

    order concert

---
+++ app/controllers/concerts_controller.rb
@@ -0,0 +1,51 @@
+class ConcertsController < ApplicationController
+
+	def index
+		@concerts = Concert.order('show_date ASC')
+	end
+
+	def show
+		@concert = Concert.find(params[:id])
+		@comment = Comment.new
+	end
+
+	def new
+		@concert = Concert.new
+	end
+
+	def create
+		@concert = Concert.new(concert_params)
+		if @concert.save
+			redirect_to concerts_path
+		end
+	end
+
+	def edit
+		@concert = Concert.find(params[:id])
+	end
+
+	def update
+		@concert = Concert.find(params[:id])
+		if @concert.update_attributes(concert_params)
+			redirect_to @concert, notice: "Good"
+		end
+	end
+
+	def register
+	  @concert = Concert.find(params[:id])
+	  user = User.find(params[:user_id])
+	  @concert.users << user # adding the user
+	end
+
+
+	def unsubscribe
+	 @concert = Concert.find(params[:id])
+	  user = User.find(params[:user_id])
+	  @concert.users.delete(user)
+	end
+
+	private
+	def concert_params
+		params.require(:concert).permit(:id, :show_date, :city)
+	end
+end
