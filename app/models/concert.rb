610ed9ad155b4b60761f497545de7d60862a1386
Nelly Duclos <nellyduclos8@gmail.com>
2019-03-08 01:17:00 +0100

    margin sous table

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,11 +1,8 @@
 class Concert < ApplicationRecord
  	has_and_belongs_to_many :users
 	has_many :comments, dependent: :destroy
-
-
 	validates :city, presence: true
   validates :venue, presence: true
  	validates :kountry, presence: true
-
 	validates :show_date, presence: true, uniqueness: true
 end
