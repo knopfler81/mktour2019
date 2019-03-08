92217e845d3070b2048c126383b2c710e5fed259
Nelly Duclos <nellyduclos8@gmail.com>
2019-03-08 15:54:26 +0100

    meta

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
 	has_and_belongs_to_many :concerts
 
 	def full_name
-	  "#{self.first_name} #{self.last_name}"
+	  "#{self.first_name}.capitalize  #{self.last_name}.capitalize"
 	end
 end
