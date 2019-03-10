1ce569ae231e4904bc3811e70e7c169fa7b37e2e
Nelly Duclos <nellyduclos8@gmail.com>
2019-03-10 13:52:31 +0100

    session

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-
-Rails.application.config.session_store :cookie_store, key: '_mktour_session', domain: {
-   production: 'mktour2019.herokuapp.com',
-   development: 'localhost:3000'
- }.fetch(Rails.env.to_sym, :all)
