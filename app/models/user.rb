fdeb6b3ec2238040e11f777eae397cdeb3ac46f1
Nelly Duclos <nellyduclos8@gmail.com>
2019-03-10 01:12:30 +0100

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
 
 	mount_uploader :avatar, AvatarUploader
 
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+
 	def full_name
 	  "#{self.first_name.capitalize } #{self.last_name.capitalize }"
 	end
